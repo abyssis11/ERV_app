1438facb9a89e4aa99cf9193388aa94f0f544a6c
Deni <denikernjus@gmail.com>
2023-02-17 12:54:00 +0100

    models update

--- ERV/main/admin.py
+++ ERV/main/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from main.models import *
 
 # Register your models here.
-models_list=[Worker, Job, EnterReadValue, ExitReadValue, ERV]
+models_list=[Worker, Job, ERV]
 admin.site.register(models_list)
--- ERV/main/models.py
+++ ERV/main/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 from datetime import date, datetime 
 
-DESCRIPTION_CHOICES = {
+CATEGORY_CHOICES = {
     ('administracija', 'administracija'),
     ('nastavnici', 'nastavnici')
+}
+
+FLAG_CHOICES = {
+    ('rednovni rad', 'RR'),
+    ('putni nalog', 'PN'),
+    ('bolovanje', 'BO'),
+    ('slobodan dan', 'SL'),
+    ('godisnji odmor', 'GO'),
+    ('rad od kuce', 'RK')
 }
 
 
 class Job(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=15, choices=DESCRIPTION_CHOICES)
+    category = models.CharField(max_length=15, choices=CATEGORY_CHOICES)
    
     def __str__(self):
         return self.name
@@ -19,31 +28,19 @@ class Worker(models.Model):
     surname = models.CharField(max_length=30)
     job = models.ForeignKey(Job, on_delete=models.CASCADE) 
 
-
     def __str__(self):
         return self.name + ' ' + self.surname
 
-class ExitReadValue(models.Model):
-    current_day = models.DateField(default=date.today)
-    current_time = models.TimeField()
-    building = models.CharField(max_length=50)
-   
-    def __str__(self):
-        return str(self.current_day) + ' ' + str(self.current_time)
-
-class EnterReadValue(models.Model):
-    current_day = models.DateField(default=date.today, null=True, blank=True)
-    current_time = models.TimeField(null=True, blank=True)
-    building = models.CharField(max_length=50, null=True, blank=True)
-   
-    def __str__(self):
-        return str(self.current_day) + ' ' + str(self.current_time) 
 
 class ERV(models.Model):
-    delta_time = models.TimeField()
-    flag = models.CharField(max_length=2)
-    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
-    enter_read_value = models.OneToOneField(EnterReadValue, on_delete=models.CASCADE, null=True, blank=True)
-    exit_read_value = models.OneToOneField(ExitReadValue, on_delete=models.CASCADE, null = True)
+    worker = models.ForeignKey(Worker, on_delete=models.DO_NOTHING) # on_delete=models.CASCADE
+    current_date = models.DateField(default=date.today)
+    enter_time = models.TimeField(null=True, blank=True)
+    exit_time = models.TimeField(null=True, blank=True)
+    flag = models.CharField(max_length=14, choices=FLAG_CHOICES)
+
+    class Meta:
+        unique_together = ('worker', 'current_date')
+
     def __str__(self):
-        return self.worker.name + ' ' + self.worker.surname + ' ' + str(self.enter_read_value.current_day) + ' ' + str(self.enter_read_value.current_time) + ' - ' + str(self.exit_read_value.current_time)
+        return self.worker.name + ' ' + self.worker.surname + ' ' +  str(self.current_date) + ' ' + str(self.enter_time) + ' - ' + str(self.exit_time)
