b139dec795880146d2f3bbd6c3815712ab5f4a0b
Deni <denikernjus@gmail.com>
2023-02-27 20:36:07 +0100

    comments

--- ERV/main/management/commands/loadingCSV.py
+++ ERV/main/management/commands/loadingCSV.py
@@ -8,9 +8,11 @@ from main.models import *
 ADMINISTRACIJA = ['SPREMAČICE', 'SIC'] # NADOPUNITI
 FORMAT_DATE = '%d.%m.%Y.'
 FORMAT_TIME = '%H:%M:%S'
+# last marker / last visited index in csv file
 MARKER = Marker.objects.last()
 
 with open('/app/ERV/main/management/commands/csv_file.csv', 'r+') as csv_file:
+    # continuing from last marker
     csv_file.seek(MARKER.value+1)
     csv_reader = csv.reader(csv_file, delimiter=',')
 
@@ -27,13 +29,13 @@ with open('/app/ERV/main/management/commands/csv_file.csv', 'r+') as csv_file:
         file_date = row[3]
         times.append(row[4])
 
-    # ako je file_date ostao 0 onda znaci da je csv_reader prazan
+    # if file_data is 0 then csv_reader is empty and there is nothing to save/read
     if file_date:
         class Command(BaseCommand):
             @transaction.atomic
             def handle(self, *args, **kwargs):
                 for i in range(len(jobs)):
-                    # ako ne postoji posao u bazi dodajemo ga
+                    # adding new job to db
                     if not Job.objects.filter(name__iexact=jobs[i]).exists():
                         if jobs[i].upper() in ADMINISTRACIJA:
                             Job(name = jobs[i], category='administracija').save()
@@ -41,35 +43,37 @@ with open('/app/ERV/main/management/commands/csv_file.csv', 'r+') as csv_file:
                             Job(name = jobs[i], category='nastavnici').save()
                             
                 for i in range(len(surnames)):
-                    # ako ne postoji osoba u bazi dodajemo ju
+                    # adding new worker to db
                     if not Worker.objects.filter(name__iexact=names[i]).exists() or not Worker.objects.filter(surname__iexact=surnames[i]).exists(): 
                         Worker(name = names[i], surname = surnames[i], job = Job.objects.get(name__iexact=jobs[i])).save()
-                    # ako je u bazi ali je ne aktivna, stavljamo je u aktivnu
+                    # if worker is in db, but not active, we make it active 
                     else:
                         worker = Worker.objects.get(Q(name__iexact=names[i]) and Q(surname__iexact=surnames[i]))
                         if not worker.active:
                             worker.active=True
                         worker.save()
-                # za svakog aktivnog radnika dodajemo ERV za taj dan
+
+                # for every active worker, the new ERV(record of working hours) for that day is added
                 for w in Worker.objects.all():
                     if w.active:
                         ERV(worker = w, current_date = datetime.strptime(file_date, FORMAT_DATE)).save()
-                # popunjavamo ERV za svakog radnika
+
+                # filing ERV data
                 for i in range(len(surnames)):
-                    # uzimamo ERV s današnjim danom i imenom i prezimenom radnika
                     erv = ERV.objects.get(current_date=datetime.strptime(file_date, FORMAT_DATE), worker = Worker.objects.get(Q(name__iexact=names[i]) and Q(surname__iexact=surnames[i])))
-                    # ako je enter time null i ako je vrijeme manje od 12h onda je to enter time
+                    # if enter time is null and the time is lower then 12h then that is enter time
                     if not erv.enter_time and datetime.strptime(times[i], FORMAT_TIME) < datetime.strptime('12:00:00', FORMAT_TIME):
                         erv.enter_time = datetime.strptime(times[i], FORMAT_TIME)
                         erv.flag='redovni rad'
                     else:
-                        # ako postoji prvo vrijeme nakon 12h onda je to zapravo enter time
+                        # if first time is after 12h that is actually enter time
                         if erv.exit_time and not erv.enter_time:
                             erv.enter_time=erv.exit_time
                         erv.exit_time = datetime.strptime(times[i], FORMAT_TIME)
                         erv.flag='redovni rad'
                     erv.save()
 
+        # saving new position to marker
         currentMarker = csv_file.tell()
         Marker(value = currentMarker).save()
 
