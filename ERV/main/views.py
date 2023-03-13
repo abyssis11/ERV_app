d2e91b8b6ff7d07a225cfb0a233a4c56bd7ef0aa
Deni <denikernjus@gmail.com>
2023-03-13 18:36:06 +0100

    fixing dockerfile

--- ERV/main/views.py
+++ ERV/main/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
 from main.models import ERV
+from django.http import HttpResponse
 
 # Create your views here.
-
 class WorkerList(ListView):
     model = ERV
-
-from django.http import HttpResponse
-# Create your views here.
 
 def homepage(request):
     return render(request, 'base_generic.html')
