347ac5657c1b369bc5202e384610aded016927be
Damjan Kraljić <d.kraljic49@gmnail.com>
2023-03-13 17:13:13 +0100

    Worker list, search bar component added, views modified

--- ERV/main/views.py
+++ ERV/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+from main.models import ERV
 
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'base_generic.html'
+
+class WorkerList(ListView):
+    model = ERV
+
