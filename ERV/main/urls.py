ac0e9eaabce24fc031ab67cd3865c01fdfb9272e
damii01 <damii01@github.com>
2023-03-14 14:30:14 +0100

    implemented search_bar, filtering db items by name, surname. Added select item for month filtering

--- ERV/main/urls.py
+++ ERV/main/urls.py
@@ -5,4 +5,11 @@ app_name = 'main'
 
 urlpatterns = [
     path('homepage', views.homepage, name='homepage'),
+    path('worker_list/', views.worker_list, name="worker_list"),
 ]
+
+htmx_urlpatterns = [
+    path('search-worker/', views.search_worker, name='search-worker'),
+]
+
+urlpatterns += htmx_urlpatterns
--- ERV/main/views.py
+++ ERV/main/views.py
@@ -1,11 +1,44 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
-from main.models import ERV
+from main.models import ERV, Worker
 from django.http import HttpResponse
+from django.db.models import Q 
+import logging
 
 # Create your views here.
-class WorkerList(ListView):
-    model = ERV
-
 def homepage(request):
     return render(request, 'base_generic.html')
+
+
+def search_worker(request):
+    searched_word = request.POST.get('search_worker')
+    
+    selected_month = request.GET.get('month')
+    ERVs = ERV.objects.all()
+
+    if searched_word != "":
+        for term in searched_word.split():
+            worker = ERVs.filter( Q(worker__name__icontains = term) | Q(worker__surname__icontains = term))
+    else:
+        worker = ""
+    
+    if selected_month != "":
+        worker = ERVs.filter(current_date__month = selected_month)
+
+
+    # if request.POST.get('month'):
+    #     worker = worker.filter(current_dat__month = selected_month)
+
+    results = worker
+
+    context = {'results': results}
+    return render(request, 'partials/result_searched_workers_tables.html', context)
+
+
+def worker_list(request):
+    workers= ERV.objects.all()
+
+    context = {'workers': workers}
+    return render(request, 'main/worker_list.html', context=context)
+
+
