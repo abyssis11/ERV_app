1f0e340d13307fd91a1499a7d4d7befcb3106c83
abyssis11 <denikernjus@gmail.com>
2023-03-14 23:29:49 +0100

    uploading csv file works

--- ERV/main/urls.py
+++ ERV/main/urls.py
@@ -5,4 +5,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('homepage', views.homepage, name='homepage'),
+    path('upload_CSV', views.upload_CSV, name='upload_CSV'),
+    path('upload', views.upload, name='upload')
 ]
