dd9a6895bdb11c48d9052671257b16a0856ea69d
damii01 <damii01@github.com>
2023-03-20 20:11:14 +0100

    completed filtering by user, date range, month and category. Added calendar and category select. Minor CSS changes.

--- ERV/main/urls.py
+++ ERV/main/urls.py
@@ -4,12 +4,14 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('homepage', views.homepage, name='homepage'),
+    path('', views.homepage, name='homepage'),
     path('worker_list/', views.worker_list, name="worker_list"),
+    path('filter/', views.filter, name="filter")
 ]
 
 htmx_urlpatterns = [
     path('search-worker/', views.search_worker, name='search-worker'),
+    path('category_list/', views.category_list, name='category_list'),
 ]
 
 urlpatterns += htmx_urlpatterns
--- ERV/main/views.py
+++ ERV/main/views.py
@@ -3,34 +3,80 @@ from django.views.generic import ListView, TemplateView
 from main.models import ERV, Worker
 from django.http import HttpResponse
 from django.db.models import Q 
-import logging
+import re
+from datetime import datetime
 
 # Create your views here.
 def homepage(request):
     return render(request, 'base_generic.html')
 
+def category_list(request):
+    jobs = Job.objects.all()
+    context = {'jobs': jobs}
+    return render(request, 'partials/category_selection.html', context=context)
 
 def search_worker(request):
-    searched_word = request.POST.get('search_worker')
-    
-    selected_month = request.GET.get('month')
-    ERVs = ERV.objects.all()
+    searched_word = ""
+    selected_month = ""
+    selected_category = ""
+    selected_date = ""
+    k=""
+    if request.method == "POST":
+        if request.POST.get('search_worker'):
+            searched_word = request.POST.get('search_worker')
+        if request.POST.get('month'):
+            selected_month = request.POST.get('month')
+        if request.POST.get('category'):
+            selected_category = request.POST.get('category')
+        if request.POST.get('daterange'):
+            selected_date = request.POST.get('daterange')
+        
+        ERVs = ERV.objects.all()
 
-    if searched_word != "":
-        for term in searched_word.split():
-            worker = ERVs.filter( Q(worker__name__icontains = term) | Q(worker__surname__icontains = term))
-    else:
-        worker = ""
-    
-    if selected_month != "":
-        worker = ERVs.filter(current_date__month = selected_month)
+        if request.POST.get('search_worker') and request.POST.get('month') and request.POST.get('category'):
+            for term in searched_word.split():
+                worker = ERVs.filter( Q(worker__name__icontains = term) | Q(worker__surname__icontains = term))
+            worker = worker.filter(current_date__month = selected_month)
+            worker = worker.filter(worker__job__category = selected_category)
+        else:
+            if request.POST.get('search_worker') and request.POST.get('month'): 
+                for term in searched_word.split():
+                    worker = ERVs.filter( Q(worker__name__icontains = term) | Q(worker__surname__icontains = term))
+                worker = worker.filter(current_date__month = selected_month)
+          
+            elif request.POST.get('search_worker') and request.POST.get('category'):
+                for term in searched_word.split():
+                    worker = ERVs.filter( Q(worker__name__icontains = term) | Q(worker__surname__icontains = term))
+                worker = ERVs.filter(worker__job__category = selected_category)
 
+            elif request.POST.get('month') and request.POST.get('category'):
+                worker = ERVs.filter(current_date__month = selected_month)
+                worker = worker.filter(worker__job__category = selected_category)
+            
+            else:
 
-    # if request.POST.get('month'):
-    #     worker = worker.filter(current_dat__month = selected_month)
+                if request.POST.get('search_worker'):
+                    for term in searched_word.split():
+                        worker = ERVs.filter( Q(worker__name__icontains = term) | Q(worker__surname__icontains = term))
+                
+                if request.POST.get('month'):
+                    worker = ERVs.filter(current_date__month = selected_month)
+                
+                if request.POST.get('category'):
+                    worker = ERVs.filter(worker__job__category = selected_category)
+                
+                if request.POST.get('daterange'):
+                    dates = re.split(' - ', selected_date)
+                    date1 = dates[0]
+                    date2 = dates[1]
+                    worker = ERVs.filter(current_date__range = [datetime.strptime(date1, '%Y-%m-%d').date(), datetime.strptime(date2, '%Y-%m-%d').date()])
 
+        if not request.POST.get('search_worker') and not request.POST.get('month') and not request.POST.get('category'):
+            worker = ""
+    else: 
+        results = ""
     results = worker
-
+    # post = request.POST
     context = {'results': results}
     return render(request, 'partials/result_searched_workers_tables.html', context)
 
@@ -40,5 +86,8 @@ def worker_list(request):
 
     context = {'workers': workers}
     return render(request, 'main/worker_list.html', context=context)
+
+def filter(request):
+    return render(request, 'partials/filter.html')
 
 
